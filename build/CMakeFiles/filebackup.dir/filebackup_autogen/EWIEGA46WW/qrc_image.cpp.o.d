8f1d80478daef7a4125ba53accab2fd31913950a
Blizzard-cyber <369476975@qq.com>
2024-01-08 12:06:17 +0800

    improve UI v2

--- build/CMakeFiles/filebackup.dir/filebackup_autogen/EWIEGA46WW/qrc_image.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/filebackup.dir/filebackup_autogen/EWIEGA46WW/qrc_image.cpp.o: \
- /home/linux-aka/filebackup/build/filebackup_autogen/EWIEGA46WW/qrc_image.cpp \
- /usr/include/stdc-predef.h
